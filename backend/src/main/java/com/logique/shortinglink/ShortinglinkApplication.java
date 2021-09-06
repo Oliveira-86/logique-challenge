e40bb5535c4f7a139f9c67996df16a24b94c258a
oliveira-86 <egberto.duarte.14@gmail.com>
2021-09-06 20:43:26 -0300

    Backend project created

---
+++ backend/src/main/java/com/logique/shortinglink/ShortinglinkApplication.java
@@ -0,0 +1,13 @@
+package com.logique.shortinglink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortinglinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortinglinkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/logique/shortinglink/ShortinglinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logique.shortinglink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortinglinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
